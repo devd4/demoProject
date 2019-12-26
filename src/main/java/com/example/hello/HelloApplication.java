7c14a8e45dfbd148fe3c23b48c47ff412e0ee481
devd4 <devendra.singh@d4insight.com>
2019-12-26 09:19:40 +0530

    commit

---
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@EnableAutoConfiguration
+@SpringBootApplication
+public class HelloApplication {
+	
+	@RequestMapping("/")
+	String hello(){
+		return "Hello! Devendra";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+		
+	}
+
+}
